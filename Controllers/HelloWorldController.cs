3ce97ea42065f0b3d053071602618938111c053c
Klecianny Melo <kleciannymelo@gmail.com>
2025-02-19 21:23:31 -0300

    :construction: controller with params

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -10,10 +10,10 @@ public class HelloWorldController : Controller
     {
         return "This is my default action...";
     }
-
+    
     // GET: /HelloWorld/Welcome
-    public string Welcome()
+    public string Welcome(string name, int numTimes = 1)
     {
-        return "This is the Welcome action method...";
+        return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
     }
 }
